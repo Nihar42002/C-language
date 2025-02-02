c931ea79241f4b85151ff388a977fdfbbed01bd9
Nihar420 <parwaninihar11@gmail.com>
2025-02-02 11:40:34 +0530

    multiple and divide

--- calculation.c
+++ calculation.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main()
 {
-   int a,b,add;
+   int a,b,add,multiple;
+   float divide;
    printf("Enter a value = ");
    scanf("%d",&a); 
    printf("Enter a value = ");
    scanf("%d",&b); 
 
    add=a+b;
+   multiple=a*b;
+   divide=a/b;
 
    printf("Answer = %d",add);
+    printf(" \n Answer = %d",multiple);
+     printf(" \n Answer = %f",divide);
 
    return 0;
-   
+
 }
