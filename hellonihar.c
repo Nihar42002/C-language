65461b7409fb4b8565c8e9b21f97b1c2e0c2a101
Nihar420 <parwaninihar11@gmail.com>
2025-02-02 09:14:05 +0530

    add

---
+++ hellonihar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,add;
+    printf("enter value");
+    scanf("%d",&a);
+    printf("enter value");
+    scanf("%d",&b);
+add=a+b;
+printf("Answer= %d",add);
+
+
+    printf("\n hello Nihar");
+    return 0;
+    
+}
