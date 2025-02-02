db37e93e8d285b7b846b4d5b32d042ab082f0a1d
Nihar420 <parwaninihar11@gmail.com>
2025-02-02 15:23:18 +0530

    Swap

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter the Value of A=");
+    scanf("%f",&a);
+    printf("Enter the Value of B=");
+    scanf("%f",&b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("\n\n A= %f",a);
+     printf("\n\n B= %f",b);
+
+     return 0;
+}
