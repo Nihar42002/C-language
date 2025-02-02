abe1e450737902a83ba8e7e72841ff0e1b30790c
Nihar420 <parwaninihar11@gmail.com>
2025-02-02 10:17:20 +0530

    add program

---
+++ calculation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,add;
+   printf("Enter a value = ");
+   scanf("%d",&a); 
+   printf("Enter a value = ");
+   scanf("%d",&b); 
+
+   add=a+b;
+
+   printf("Answer = %d",add);
+
+   return 0;
+   
+}
