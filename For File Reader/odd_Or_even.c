e9018eb9e6a00982f3ed20f8f64539d1f6534bce
Nihar420 <parwaninihar11@gmail.com>
2025-02-24 17:29:31 +0530

    change

--- For File Reader/odd_Or_even.c
+++ For File Reader/odd_Or_even.c
@@ -5,7 +5,8 @@ int main()
     FILE *fptr, *even, *odd;
 
     fptr = fopen("text.txt", "r");
-    if (fptr == NULL) {
+    if (fptr == NULL) 
+    {
         printf("Error: Could not open text.txt\n");
         return 1; // Exit with error code
     }
@@ -13,7 +14,8 @@ int main()
     even = fopen("even.txt", "w");
     odd = fopen("odd.txt", "w");
 
-    if (even== NULL || odd == NULL) {
+    if (even== NULL || odd == NULL)
+     {
         printf("Error: Could not create output files\n");
         fclose(fptr);
         return 1;
@@ -22,10 +24,13 @@ int main()
     char ch;
 
     // Write even ASCII characters to even.txt
-    while ((ch = fgetc(fptr)) != EOF) {
-        if (ch % 2 == 0) {
+    while ((ch = fgetc(fptr)) != EOF) 
+    {
+        if (ch % 2 == 0) 
+        {
             fprintf(even, "%c", ch);
-        } else {
+        } else 
+        {
             fprintf(odd, "%c\t", ch);
         }
     }
