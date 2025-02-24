954c35109da7ea38498d3a84f73016941d5432c6
Nihar420 <parwaninihar11@gmail.com>
2025-02-24 14:02:57 +0530

    Odd or even

---
+++ For File Reader/odd_Or_even.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main()
+{
+FILE *fptr;
+
+fptr=fopen("text.txt","r");
+
+if (fptr == NULL) {
+   printf("Error: Could not open file.\n");
+   return 1; // Exit with error code
+}
+
+char ch;
+
+printf("EVEN: \n");
+printf("---------------------------\n");
+while((ch=fgetc(fptr))!=EOF)
+ {
+    if(ch%2==0||ch==0)
+    {
+    printf("%c\n",ch);
+   
+    }  
+ }
+
+ rewind(fptr);
+
+
+ printf("ODD: \n");
+ printf("---------------------------\n");
+ while((ch=fgetc(fptr))!=EOF)
+ {
+    if(ch%2!=0||ch==0)
+    {
+    printf("%c\n",ch);
+   
+    }  
+   
+ }
+
+
+ fclose(fptr);
+ return 0;
+
+}
